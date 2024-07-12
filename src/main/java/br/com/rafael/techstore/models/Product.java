ada151e2da63ea0091a5cafed9cdd90d7d548601
Rafael Gasparoto <rafaelgasparoto4@gmail.com>
2024-07-12 14:09:10 -0300

    Migration popular tabela products
    - Alterado classe Product para os novos parâmetros
    - Criado migration de insert em products

--- src/main/java/br/com/rafael/techstore/models/Product.java
+++ src/main/java/br/com/rafael/techstore/models/Product.java
@@ -16,15 +16,75 @@ public class Product implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(name = "category_id", nullable = false)
+    private Integer categoryId;
+
+    @Column(name = "name", nullable = false)
     private String name;
 
-    public Product() {
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price", nullable = false)
+    private Float price;
+
+    @Column(name = "promo_price")
+    private Float promoPrice;
+
+    @Column(name = "stock", nullable = false)
+    private Integer stock;
+
+    public Product(){}
+
+    public Product(Long id, Integer categoryId, String name, String description, Float price, Float promoPrice,
+            Integer stock) {
+        this.id = id;
+        this.categoryId = categoryId;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.promoPrice = promoPrice;
+        this.stock = stock;
     }
 
-    public Product(Long id, String name) {
-        this.id = id;
-        this.name = name;
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public Float getPromoPrice() {
+        return promoPrice;
+    }
+
+    public void setPromoPrice(Float promoPrice) {
+        this.promoPrice = promoPrice;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
     }
 
     public Long getId() {
@@ -43,4 +103,34 @@ public class Product implements Serializable {
         this.name = name;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((categoryId == null) ? 0 : categoryId.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Product other = (Product) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (categoryId == null) {
+            if (other.categoryId != null)
+                return false;
+        } else if (!categoryId.equals(other.categoryId))
+            return false;
+        return true;
+    }
 }
---
+++ src/main/resources/db/migration/V2__Insert_Data_Products.sql
@@ -0,0 +1,47 @@
+INSERT INTO
+    products (
+        id,
+        category_id,
+        name,
+        description,
+        price,
+        promo_price,
+        stock
+    )
+VALUES
+    (
+        1,
+        1,
+        'MEMORIA PCYES 8GB (1X8) DDR3 1600MHZ, 32291',
+        'A Memória Kingspec de 16GB DDR4 é uma excelente escolha para quem busca alta capacidade e desempenho em seus sistemas. Com 16GB de capacidade e frequência de 3200MHz, este módulo de memória é ideal para jogos, multitarefa e aplicações profissionais que exigem velocidade e eficiência.',
+        89.9,
+        38.93,
+        4
+    ),
+    (
+        2,
+        1,
+        'MEMORIA HIKSEMI ARMOR, (1X16G), DDR4, 3200MHZ, ROXO, HSC416U32Z2-16G',
+        'A Memória Hiksemi Armor de 16GB DDR4 combina desempenho robusto e design distinto. Com capacidade de 16GB, frequência de 3200MHz, latência CAS baixa e um design elegante em roxo, este módulo de memória é ideal para gamers, criadores de conteúdo e entusiastas de PC que buscam eficiência e estilo.',
+        84.49,
+        67.44,
+        64
+    ),
+    (
+        3,
+        2,
+        'MONITOR BRX 19, 19.5 POL., 60HZ, HDMI/VGA, BRX195',
+        '',
+        62.54,
+        81.94,
+        97
+    ),
+    (
+        4,
+        2,
+        'MONITOR GAMER MSI G274CV, 27 POL, VA, FHD, 1MS, 75HZ, HDMI/DP, G274CV',
+        '',
+        83.7,
+        11.98,
+        3
+    );
